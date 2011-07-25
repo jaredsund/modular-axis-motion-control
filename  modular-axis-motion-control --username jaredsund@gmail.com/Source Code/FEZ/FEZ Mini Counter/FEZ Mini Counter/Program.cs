07019be4a695abcf99dab12f6d4f99efc17cbee0
jaredsund@gmail.com <jaredsund@gmail.com@4f1ce6b3-515f-692d-006c-d58bde3a8fda>
2011-07-25 08:54:29 +0000

    Mini with timer3 working, interrupt, and event handling

---  modular-axis-motion-control --username jaredsund@gmail.com/Source Code/FEZ/FEZ Mini Counter/FEZ Mini Counter/Program.cs
+++  modular-axis-motion-control --username jaredsund@gmail.com/Source Code/FEZ/FEZ Mini Counter/FEZ Mini Counter/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+ * This code demonstrate how to use timer3 on the FEZ Mini to create an externally 
+ * triggered high speed counter with interrupt and event handling.  
+ * 
+ * See: http://www.keil.com/dd/docs/datashts/philips/lpc23xx_um.pdf
+ * See: http://www.ghielectronics.com/downloads/FEZ/Mini/FEZMini_sch.pdf
+ * 
+ * Written by Jared Sund
+ */
+
+using System;
 using System.Threading;
 
 using Microsoft.SPOT;
@@ -13,181 +23,172 @@ namespace FEZ_Mini_Counter
 {
     public class Program
     {
-        private static Register T3TCR;
-        private static Register T3IR;
+        private static Register T3CR;   //Timer Control Register
+        private static Register T3IR;   //Interrupt Register 
 
         static void IntButton_OnInterrupt(uint port, uint state, DateTime time)
         {
-            T3TCR.SetBits((1 << 1));
-            T3TCR.ClearBits((1 << 1));
-            T3IR.Write(1 << 2);
-        }
-
-        static void IntDI7_OnInterrupt(uint port, uint state, DateTime time)
-        {
-            Debug.Print("INTDI7 Interrupt");
+            //reset
+            T3CR.SetBits((1 << 1));
+            T3CR.ClearBits((1 << 1));
+            T3IR.Write(1 << 0);
         }
 
         static void IntDI8_OnInterrupt(uint port, uint state, DateTime time)
         {
-            Debug.Print("INTDI8 Interrupt");
+            //The external match register caused an interrupt
+            Debug.Print("INTDI8 Interrupted");
         }
 
         public static void Main()
         {
-
             //Disable the garbage collector messages
             Debug.EnableGCMessages(false);
 
+            //used to generate our external timer input, could be an encoder.
             PWM pwm = new PWM((PWM.Pin)FEZ_Pin.PWM.Di3);
             pwm.Set(false);
             pwm.Set(2, 50);
 
-            //http://www.keil.com/dd/vtr/4531/9857.htm
 
+            //The LDR button is used as a physical counter and interupt reset
             InterruptPort IntButton = new InterruptPort((Cpu.Pin)FEZ_Pin.Interrupt.LDR, true,
                                    Port.ResistorMode.PullUp,
                                    Port.InterruptMode.InterruptEdgeBoth);
-
-            // add an interrupt handler to the pin
             IntButton.OnInterrupt += new NativeEventHandler(IntButton_OnInterrupt);
 
-            //InterruptPort IntDI7 = new InterruptPort((Cpu.Pin)FEZ_Pin.Interrupt.Di7 , true,
-            //                       Port.ResistorMode.PullUp,
-            //                       Port.InterruptMode.InterruptEdgeBoth);
 
-            //// add an interrupt handler to the pin
-            //IntDI7.OnInterrupt += new NativeEventHandler(IntDI7_OnInterrupt);
-
-            //InterruptPort IntDI8 = new InterruptPort((Cpu.Pin)FEZ_Pin.Interrupt.Di8 , true,
-            //                       Port.ResistorMode.PullUp,
-            //                       Port.InterruptMode.InterruptEdgeBoth);
-
-            //// add an interrupt handler to the pin
-            //IntDI8.OnInterrupt += new NativeEventHandler(IntDI8_OnInterrupt);
+            //DI8 is used as the MAT3.0 pin, interrupted by the External Match Register
+            InterruptPort IntDI8 = new InterruptPort((Cpu.Pin)FEZ_Pin.Interrupt.Di8, true,
+                                   Port.ResistorMode.PullUp,
+                                   Port.InterruptMode.InterruptEdgeBoth);
+            IntDI8.OnInterrupt += new NativeEventHandler(IntDI8_OnInterrupt);
 
 
-            
+            /*PINSEL0 - 0xE002 C000
+             * 21:20 P0.10 GPIO Port 0.10 TXD2 SDA2 MAT3.0 00
+             * see page 157, LPC23XX User manual Rev. 02 — 11 February 2009
+             * 
+             * Use the Pin function select register PINSEL0, to enable MAT3.0
+             * on P0.10.  This is used for the External Match Register
+            */
+            Register PINSEL0 = new Register(0xE002C000);
+            PINSEL0.Write(3 << 20);
 
-
-            //this is the power control register (periferials) - PCONP
-            Register PCONP = new Register(0xE01FC0C4);
-            PCONP.SetBits(1 << 23);//enable timer3 
-
-
-            // Select An0 on Mini (Cap3.0)
-            Register PINSELCAP30 = new Register(0xE002C004);
-            PINSELCAP30.SetBits((3 << 14));//set bits 14 and 15
-
-
-            // To enable timer/counter
-            //TCR Timer Control Register 
-            //timers are 0,1,2,3 - this one is timer 3
-            T3TCR = new Register(0xE0074004);
-            T3TCR.Write(1);
-            /*
-             * Timer Control Register, The Timer Control Register (TCR) is used to control the operation of the Timer/Counter.
-             * See page 554, LPC23XX User manual Rev. 02 — 11 February 2009
+            /*Power Control for Peripherals register (PCONP - 0xE01F C0C4)
+             * 23 PCTIM3 Timer 3 power/clock control bit.
+             * see pages 68-69, LPC23XX User manual Rev. 02 — 11 February 2009
+             * 
+             * In the Power Control for Peripherals register, turn on bit 23
+             * to turn power on for timer3
              */
+            Register PCONP = new Register(0xE01FC0C4);
+            PCONP.SetBits(1 << 23);
 
-            // set prescale to 0
+            /*Pin Function Select Register 1 (PINSEL1 - 0xE002 C004)
+             * 15:14 P0.23 GPIO Port 0.23 AD0.0 I2SRX_CLK CAP3.0 00
+             * see page 158, LPC23XX User manual Rev. 02 — 11 February 2009
+             * 
+             * Use the Pin finction select register PINSEL1, to enable Cap3.0
+             * on P0.23.  Select An0 on Mini (Cap3.0) - An0 - Mapped to the Cap3.0
+             * This will be the external input for the timer, used with the Capture Control Register
+             */
+            Register PINSEL1 = new Register(0xE002C004);
+            PINSEL1.SetBits((3 << 14));//set bits 14 and 15
+
+            /*Timer Control Register (T[0/1/2/3]CR - 0xE000 4004, 0xE000 8004,0xE007 0004, 0xE007 4004)
+             * 0 Counter Enable When one, the Timer Counter and Prescale Counter are enabled for counting. 
+             * When zero, the counters are disabled
+             * see pages 554-555, LPC23XX User manual Rev. 02 — 11 February 2009
+             * 
+             * This will enable timer3
+             */
+            T3CR = new Register(0xE0074004);
+            T3CR.Write(1);
+
+            /*TPrescale register (T0PR - T3PR, 0xE000 400C, 0xE000 800C, 0xE007 000C, 0xE007 400C)
+             * The 32-bit Prescale register specifies the maximum value for the Prescale Counter.
+             * see page 556, LPC23XX User manual Rev. 02 — 11 February 2009
+             * 
+             * This will set the prescale to 0 for timer3
+             */
             Register T3PR = new Register(0xE007400C);
             T3PR.Write(0);
 
-            /*
-             * Prescale register: The 32-bit Prescale register specifies the maximum value for the Prescale Counter.
-             * See page 556, LPC23XX User manual Rev. 02 — 11 February 2009
+            /*Count Control Register (T[0/1/2/3]CTCR - 0xE000 4070, 0xE000 8070, 0xE007 0070, 0xE007 4070)
+             * 10 Counter Mode: TC is incremented on falling edges on the CAP input selected by bits 3:2.
+             * see page 555, LPC23XX User manual Rev. 02 — 11 February 2009
+             * 
+             * This will count on falling edge of CAP3.0, An0(mini)
              */
-
             Register T3CTCR = new Register(0xE0074070);
-            T3CTCR.Write(2 << 0 | 0 << 2);//count on falling edge and use CAP3.0
-            /*
-             * Count Control Register
-             * See Page 555, LPC23XX User manual Rev. 02 — 11 February 2009
-             */
+            T3CTCR.Write(2 << 0 | 0 << 2);
 
-            // should be 0 for a counter
+            /*Capture Control Register (T[0/1/2/3]CCR - 0xE000 4028, 0xE000 8028, 0xE007 0028, 0xE007 4028)
+             * see page 558, LPC23XX User manual Rev. 02 — 11 February 2009
+             * 
+             * This is set to 0, for a counter
+             */
             Register T3CCR = new Register(0xE0074028);
             T3CCR.ClearBits(0x07);
-            /*
-             * Capture Control Register. The CCR
-             * controls which edges of the capture inputs
-             * are used to load the Capture Registers
-             * and whether or not an interrupt is
-             * generated when a capture takes place.
-             * 
-             * Each Capture register is associated with a device pin and may be loaded with the Timer
-             * Counter value when a specified event occurs on that pin. The settings in the Capture
-             * Control Register register determine whether the capture function is enabled, and whether
-             * a capture event happens on the rising edge of the associated pin, the falling edge, or on
-             * both edges
-             * 
-             * see Page 558, LPC23XX User manual Rev. 02 — 11 February 2009
-             */
-
             // To reset the counter
-            T3TCR.SetBits((1 << 1));
-            T3TCR.ClearBits((1 << 1));
+            T3CR.SetBits((1 << 1));
+            T3CR.ClearBits((1 << 1));
 
             /*------------------------------------------------------------------*/
-            /*
-             * MCR Match Control Register. The MCR is used to control if an interrupt is generated and if 
-             * the TC is reset when a Match occurs. R/W 0 
-             * T0MCR - 0xE000 4014
-             * T1MCR - 0xE000 8014
-             * T2MCR - 0xE007 0014
-             * T3MCR - 0xE007 4014
-             * */
 
+            /*Match Control Register (T[0/1/2/3]MCR - 0xE000 4014, 0xE000 8014, 0xE007 0014, 0xE007 4014)
+             * 0 MR0I 1 Interrupt on MR0: an interrupt is generated when MR0 matches the value in the TC
+             * see page 557, LPC23XX User manual Rev. 02 — 11 February 2009
+             * 
+             * This will generate a interrupt when MR0 and TC match
+             */
             Register T3MCR = new Register(0xE0074014);
-            T3MCR.Write(1 << 6);
+            T3MCR.Write(1 << 0);
 
-            /*
-             * Bit
-             * 6 MR2I 1 Interrupt on MR2: an interrupt is generated when MR2 matches the value in the TC.
-             *  0 This interrupt is disabled
-             * 7 MR2R 1 Reset on MR2: the TC will be reset if MR2 matches it. 0
-             *  0 Feature disabled.
-             * 8 MR2S 1 Stop on MR2: the TC and PC will be stopped and TCR[0] will be
-             *  0 set to 0 if MR2 matches the TC.
+            /*MR0 Match Register
+             * see page 553, LPC23XX User manual Rev. 02 — 11 February 2009
+             * 
+             * This is value that will be tested against the TC
              */
+            Register T3MR0 = new Register(0xE0074018);
+            T3MR0.Write(10); // change this value as needed
 
-
-
-            /*
-             * Match Register 2. MR2 can be enabled through the MCR to reset the TC, stop 
-             * both the TC and PC, and/or generate an interrupt every time MR0 matches the TC.
-             */
-            Register T3MR2 = new Register(0xE0074020);
-            T3MR2.Write(10);
-
-            //T2IR - 0xE007 4000  Interrupt Register for timer3
+            /*Interrupt Register (T[0/1/2/3]IR - 0xE000 4000, 0xE000 8000, 0xE007 0000, 0xE007 4000)
+             * MR0 Interrupt Interrupt flag for match channel 0.
+            * see page 554, LPC23XX User manual Rev. 02 — 11 February 2009
+            * 
+            * This provides the interrupt on MR0
+            */
             T3IR = new Register(0xE0074000);
-            T3IR.Write(1 << 2);
+            T3IR.Write(1 << 0);
 
-            Register GIOI = new Register (0xE0028080);
-            Register EM3 = new Register (0xE007403C);
-            EM3.Write(3<<8);
-            //EM3.Write(1 << 11);
+            /*External Match Register (T[0/1/2/3]EMR - 0xE000 403C, 0xE000 803C, 0xE007 003C, 0xE007 403C)
+             * see page 559, LPC23XX User manual Rev. 02 — 11 February 2009
+             * 
+             * Handel the interrupt on EM0 (MAT3.0)
+             * Setting bits 5:4 high, toggles the corresponding External Match bit/output (MAT3.0).
+             */
+            Register EM0 = new Register(0xE007403C);
+            EM0.Write(3 << 4);
 
-
-            // To read
+            /*Timer Counter registers (T0TC - T3TC, 0xE000 4008, 0xE000 8008, 0xE007 0008, 0xE007 4008)
+             * see page 556, LPC23XX User manual Rev. 02 — 11 February 2009
+             * 
+             * This is the count of Timer3
+             */
             Register T3TC = new Register(0xE0074008);
+
             while (true)
             {
-                // if(T2IR.Read () ==1)
-                //{
                 Debug.Print("Total count: " + T3TC.Read());
                 Debug.Print("IR: " + T3IR.Read());
-                Debug.Print("");
-                Debug.Print(EM3.Read().ToString());
+                Debug.Print("External Match Register: " + EM0.Read().ToString());
 
                 Thread.Sleep(10);
-                //}
-            }
+            }//end of while loop
 
+        }//end of Main()
 
-        }
-
-    }
-}
+    }//end of class
+}//end of namespace
